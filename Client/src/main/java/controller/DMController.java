c625e052e1bfb793b578a07ab34dc3ad1f97ef74
SahandNoey <sahandnoey7798@gmail.com>
2022-07-10 00:33:53 +0430

    friendDM.fxml created

---
+++ Client/src/main/java/controller/DMController.java
@@ -0,0 +1,110 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class DMController {
+
+    @FXML
+    private VBox serversVBox;
+
+    @FXML
+    private Pane homeBtn;
+
+    @FXML
+    private Pane addServerBtn;
+
+    @FXML
+    private VBox accountAndDMVBox;
+
+    @FXML
+    private Accordion dmAccordion;
+
+    @FXML
+    private TitledPane dmTitlePane;
+
+    @FXML
+    private AnchorPane dmAnchorPane;
+
+    @FXML
+    private VBox msgAndSettingsVBox;
+
+    @FXML
+    private Pane friend1Pane;
+
+    @FXML
+    private Pane friend1StautsPane;
+
+    @FXML
+    private Label friend1NameLabel;
+
+    @FXML
+    private Label friend1StatusLabel;
+
+    @FXML
+    private VBox dmMessagesVBox;
+
+    @FXML
+    private Pane msgPane;
+
+    @FXML
+    private HBox msgHBox;
+
+    @FXML
+    private Pane msgImagePane;
+
+    @FXML
+    private ImageView msgImageView;
+
+    @FXML
+    private Label msgTxtField;
+
+    @FXML
+    private Pane friend1Pane11;
+
+    @FXML
+    private TextField dmTxtFld;
+
+    @FXML
+    private ImageView dmSendBtn;
+
+    @FXML
+    void addServerClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dmAccordionClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dmSendBtnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void friendsClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void homeBtnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void myAccountClicked(MouseEvent event) {
+
+    }
+
+}
