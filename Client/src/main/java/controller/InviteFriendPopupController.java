ccc4fb804c0fded2f847040ec743afe15c983c24
SahandNoey <sahandnoey7798@gmail.com>
2022-07-10 21:23:01 +0430

    inviteFriendsPopup.fxml created and serverMainPage.fxml updated

---
+++ Client/src/main/java/controller/InviteFriendPopupController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+
+public class InviteFriendPopupController {
+
+    @FXML
+    private TextField friendSearchTxtField;
+
+    @FXML
+    private VBox friendsVBox;
+
+    @FXML
+    private Pane eachFriendPane;
+
+    @FXML
+    private Circle eachFriendCircle;
+
+    @FXML
+    private Label eachFriendName;
+
+    @FXML
+    void searchFriendClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sendBtnClicked(MouseEvent event) {
+
+    }
+
+}
